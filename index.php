6112a75310feeca6bb787e895868a18f9abd9919
khotlen <khotlen@hotmail.com>
2015-09-18 19:03:16 -0400

    Open link in new tab rather than replace current tab

--- index.php
+++ index.php
@@ -121,8 +121,8 @@
 			<footer id="footer">
 
 				<ul class="icons">
-					<li><a href="https://www.facebook.com/lakertech" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>
-					<li><a href="https://github.com/YamiND/LTI_Centric_v2" class="icon circle fa-github"><span class="label">Github</span></a></li>
+					<li><a href="https://www.facebook.com/lakertech" class="icon circle fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
+					<li><a href="https://github.com/YamiND/LTI_Centric_v2" class="icon circle fa-github" target="_blank"><span class="label">Github</span></a></li>
 				</ul>
 
 				<ul class="copyright">
