60f1f1f01dbe229bad34bfce0fb1b7f95ed02cd3
khotlen <khotlen@hotmail.com>
2015-09-18 20:13:19 -0400

    All lower case / grammar / grammar / grammar / grammar / grammar / link opens new tab instread of replacing tab

--- lti_about.php
+++ lti_about.php
@@ -40,7 +40,7 @@
 				<header class="special container">
 					<span class="icon fa-mobile"></span>
 					<h2>Laker Technology <strong>Innovations</strong></h2>
-					<p>Where technology meets business</p>
+					<p>where technology meets business</p>
 				</header>
 
 				<!-- One -->
@@ -51,7 +51,7 @@
 									<header>
 										<h3>About the Founders</h3>
 									</header>
-									<p>LTI Members have very unique and diverse backgrounds. To find out more, please click the link below:</p>
+									<p>LTI Members have very unique and diverse backgrounds.  To find out more, please click the link below:</p>
 									<footer>
 										<ul class="buttons vertical">
 							             <li><a href="#lti_members" class="button fit scrolly">Tell Me More</a></li>
@@ -66,7 +66,7 @@
 									<header>
 										<h3>About Centric</h3>
 									</header>
-									<p>Centric is a set of software utilities aimed at businesses. To find out more, please see the link below:</p>
+									<p>Centric is our very own backup and hosting service.  To find out more, please see the link below:</p>
 									<footer>
 										<ul class="buttons vertical">
 							                 <li><a href="#lti_centric" class="button fit scrolly">Tell Me More</a></li>
@@ -103,12 +103,12 @@
 							<div class="content">
 								<section>
                                     <article id="lti_members">
-                                    <h3>About our Members</h3>
+
 									<a href="#" class="image featured"><img src="/images/LTIgroup.jpg" alt="LTI members" /></a>
 									<header>
 										<h3>About our Members</h3>
 									</header>
-									<p>Laker Tech Innovations  is a start up in  Sault Ste Marie, MI connecting business and technology together. Starting in 2014 LTI has created a strong infrastructure for businesses large or small to help promote technological growth in Northern Michigan by providing data centers, website design, and server hosting. Our goal is to build relationships with companies. We love what we do and are willing to go the extra mile to get things done. Our main data center location is located in Sault Ste Marie, Mi at the Ssmartzone Building. At Laker Tech innovations we focus on the customers and work with clients to create intelligent solutions.  </p>
+									<p>Laker Tech Innovations is a start-up in Sault Ste Marie, Michigan connecting businesses and technology together.  Beginning in 2014, LTI has created a strong infrastructure for businesses, large or small, to help promote technological growth in Northern Michigan by providing data centers, website design, and server hosting.  We love what we do and we are willing to go the extra mile to get the job done. Our main data center is located in Sault Ste Marie, Michigan at the <a href="http://www.ssmartzone.com/" target="_blank">SSMartzone Building</a>.  At LTI we focus on the customers and work with clients to create intelligent solutions.</p>
                                     </article>
 								</section>
 							</div>
@@ -118,7 +118,7 @@
                                      <article id="lti_centric">
                                     <h3>About Centric</h3>
 
-									<p>Centric is a suite of software designed to allow an easy way to synchronize, backup, and restore data. We offer both a web and desktop interface to allow quick access to Centric on a multiple of devices. We have multiple plans designed to fit your business' needs. </p>
+									<p>Centric is a suite of software designed to allow an easy way to synchronize, backup, and restore data.  We offer both a web and desktop interface to allow quick access to Centric on multiple devices.  We have a variety of plans designed to fit any business's needs.</p>
                                     </article>
                                          </section>
 							</div>
@@ -130,8 +130,7 @@
 									<header>
 										<h3>Server Collocation</h3>
 									</header>
-									<p>Our services provide convenience and security for you. Laker Tech Innovations provides customers with 1u and 2u server hosting. Single server hosting provides customers with the ability to be able to control exactly the amount of space which cuts any excess cost. This is a great option for start ups and a way to micro manage bigger companies. All servers will be placed in secured racks. The safety of your data is our priority. <br>
-Our packages include:<br></p>
+									<p>Our services provide convenience and security.  LTI provides customers with 1u and 2u server hosting.  Single server hosting provides customers with the ability to control exactly the amount of space they require which cuts any excess cost.  This is a great option for start-ups and a way to micromanage bigger companies.  All servers will be placed in secured racks; the safety of data is our number-one priority. <br></p>
 
 
                                     </article>
@@ -148,8 +147,9 @@ Our packages include:<br></p>
 			<footer id="footer">
 
 				<ul class="icons">
-					<li><a href="https://www.facebook.com/lakertech" class="icon circle fa-facebook"><span class="label">Facebook</span></a></li>
-					<li><a href="https://github.com/YamiND/LTI_Centric_v2" class="icon circle fa-github"><span class="label">Github</span></a></li>
+					<li><a href="https://www.facebook.com/lakertech" class="icon circle fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
+					<li><a href="https://github.com/YamiND/LTI_Centric_v2" class="icon circle fa-github" target="_blank"><span class="label">Github</span></a></li>
+                    </a></li>
 				</ul>
 
 				<ul class="copyright">
