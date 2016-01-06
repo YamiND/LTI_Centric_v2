b6099f9bc3abb019a1c704f4125951e7956f199d
yami <comptvids@gmail.com>
2016-01-06 12:53:32 -0500

    Optimized page and sending of emails

--- contact-us.php
+++ contact-us.php
@@ -2,21 +2,70 @@
 
 if(isset($_POST['submit']))
 {
- //Email information
-  $admin_email = "contact-us@lakertech.com";
-  $email = $_POST['email'];
-  $subject = $_POST['subject'];
-  $message = $_POST['message'];
-  $name = $_POST['name'];
-  $comment = $name.$message;
+    //Email information
+    $email_to = "contact-us@lakertech.com";
+    $email_from = "website@lakertech.com";
+      
+    function died($error) 
+    {    
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "This was the error that occured:<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+    
+    //Checks if there are values
+    if(!isset($_POST['name']) || !isset($_POST['message']) || !isset($_POST['email']) || !isset($_POST['subject'])) 
+    {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+    
+    $email_subject = $_POST['subject'];
+    $message = $_POST['message'];
+    $name = $_POST['name'];
+    
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+    if(!preg_match($email_exp,$email_from)) 
+    {
+        $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+    }
 
-  //send email
-  mail($admin_email, "$subject", $comment, "From:" . $email);
+    $string_exp = "/^[A-Za-z .'-]+$/";
+ 
+    if(strlen($message) < 2) 
+    {
+        $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+    }
+ 
+    if(strlen($error_message) > 0) 
+    {
+        died($error_message);
+    }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+    function clean_string($string) 
+    {
+        $bad = array("content-type","bcc:","to:","cc:","href");
+        return str_replace($bad,"",$string);
+    }
+    
+    $email_message .= "Name: ".clean_string($name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Comments: ".clean_string($message)."\n";
+ 
+    // create email headers
+ 
+    $headers = 'From: '.$email_from."\r\n".
+    'Reply-To: '.$email_from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+ 
+    mail($email_to, $email_subject, $email_message, $headers);  
 
-  //Email response
-  echo "Thank you for contacting us!";
-
-
+    echo "Thank you for contacting us!";
 }
 
 ?>
