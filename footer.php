039cf8277401250e1cf3a6952a66195a97a4eace
yami <comptvids@gmail.com>
2016-01-06 18:07:01 -0500

    Added email icon to footer

--- footer.php
+++ footer.php
@@ -3,6 +3,7 @@ echo '<footer id="footer">
         <ul class="icons">
             <li><a href="https://www.facebook.com/lakertech" class="icon circle fa-facebook" target="_blank"><span class="label">Facebook</span></a></li>
             <li><a href="https://github.com/YamiND/LTI_Centric_v2" class="icon circle fa-github" target="_blank"><span class="label">Github</span></a></li>
+            <li><a href="https://lakertech.com/contact-us" class="icon circle fa-envelope-o" target="_blank"><span class="label">Contact Us</span></a></li>
         </ul>
         <ul class="copyright">
             <li>&copy; LTI</li>
