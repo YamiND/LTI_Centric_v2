96f635d3b7ff600d422b2e1472c7d3e516fc3d06
yami <comptvids@gmail.com>
2016-01-15 22:48:26 -0500

    Expirimental CSS include file

--- login.php
+++ login.php
@@ -38,14 +38,9 @@
         <script type="text/JavaScript" src="js/sha512.js"></script> 
         <script type="text/JavaScript" src="js/forms.js"></script> 
         
-		<noscript>
-			<link rel="stylesheet" href="css/skel.css" />
-			<link rel="stylesheet" href="css/style.css" />
-			<link rel="stylesheet" href="css/style-wide.css" />
-			<link rel="stylesheet" href="css/style-noscript.css" />
-		</noscript>
-		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
-		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+		<!-- Stylesheets -->
+            <?php include 'css.php';?>
+        
 	</head>
     
 	<body class="left-sidebar">
